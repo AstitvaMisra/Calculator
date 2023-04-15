213c402bbfc766d4b6f2358a878feea2eb38b346
AstitvaMisra <129366106+AstitvaMisra@users.noreply.github.com>
2023-04-15 17:20:36 +0530

    Add files via upload

---
+++ calculator_app/calculator.js
@@ -0,0 +1,7 @@
+// const number7 = document.getElementById('num7');
+
+// console.log("number7");
+
+const inputE1 = document.getElementById('input');
+const value1=inputE1.value;
+console.log("value1");
